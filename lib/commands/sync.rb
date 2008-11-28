a6183c7ae11224370ba84a9f3535dbe8632ad508
devyn <devyn.cairns@gmail.com>
2008-11-28 07:47:18 -0800

    colorful sync

--- lib/commands/sync.rb
+++ lib/commands/sync.rb
@@ -2,24 +2,24 @@ Leafman::Command.new "sync", "", "syncs all enabled projects with the server" do
     include Leafman::Mixin
     Leafman::Projects.each do |p|
         if (p['scm'] == 'git' and p['fetch'])
-            puts "\e[1msync-git:\e[0m #{p['name']}"
+            puts "\e[1msync-git:\e[0m \e[32m#{p['name']}\e[0m"
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
-                system("git", "pull", p['fetch']) or warn("\e[31m\e[1mcould not pull for\e[0m #{p['name']}")
+                system("git", "pull", p['fetch']) or warn("\e[31m\e[1mcould not pull for\e[0m \e[32m#{p['name']}\e[0m")
             end
         elsif p['scm'] == 'svn'
-            puts "\e[1msync-svn:\e[0m #{p['name']}"
+            puts "\e[1msync-svn:\e[0m \e[34m#{p['name']}\e[0m"
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
-                system("svn", "up") or warn("\e[31m\e[1mcould not update\e[0m #{p['name']}")
+                system("svn", "up") or warn("\e[31m\e[1mcould not update\e[0m \e[32m#{p['name']}\e[0m")
             end
         elsif (p['scm'] == 'bzr') and p['do_update']
-            puts "\e[1msync-bzr:\e[0m #{p['name']}"
+            puts "\e[1msync-bzr:\e[0m \e[33m#{p['name']}\e[0m"
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
-                system("bzr", "up") or warn("\e[31m\e[1mcould not update\e[0m #{p['name']}")
+                system("bzr", "up") or warn("\e[31m\e[1mcould not update\e[0m \e[33m#{p['name']}\e[0m")
             end
         elsif (p['scm'] == 'hg') and p['do_pull']
-            puts "\e[1msync-hg:\e[0m #{p['name']}"
+            puts "\e[1msync-hg:\e[0m \e[36m#{p['name']}\e[0m"
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
-                system("hg", "pull") or warn("\e[31m\e[1mcould not pull for\e[0m #{p['name']}")
+                system("hg", "pull") or warn("\e[31m\e[1mcould not pull for\e[0m \e[36m#{p['name']}\e[0m")
             end
         else
             next
