050e811d19ff5a4c6bb006475e9eaa4afa93a53e
devyn <devyn.cairns@gmail.com>
2008-11-29 07:45:26 -0800

    added popularity-contest

---
+++ lib/commands/popularity-contest.rb
@@ -0,0 +1,27 @@
+Leafman::Command.new "popularity-contest", "", "shows the percentages of each scm." do
+    include Leafman::Mixin
+    puts "\e[1mpopularity contest\e[0m"
+    t, g, s, b, h, d, o = [0]*7
+    Leafman::Projects.each do |p|
+        t += 1
+        case p['scm']
+            when 'git'
+                g += 1
+            when 'svn'
+                s += 1
+            when 'bzr'
+                b += 1
+            when 'hg'
+                h += 1
+            when 'darcs'
+                d += 1
+            else
+                o += 1
+        end
+    end
+    puts "\e[1mtotal projects found:\e[0m #{t}"
+    {"\e[32mGit\e[0m" => g, "\e[34mSubversion\e[0m" => s, "\e[33mBazaar\e[0m" => b, "\e[36mMercurial\e[0m" => h, "\e[35mDarcs\e[0m" => d, "Other" => o}.sort{|a1,a2|(a1[1] <=> a2[1])*-1}.each do |a|
+        k,v = a
+        puts "\e[1m#{k}\e[1m:\e[0m #{((v/t.to_f)*100).to_s[0..3]}%"
+    end
+end
