0b87b8c1bdf100a36f9c31284eebd02a7cdb138b
devyn <devyn.cairns@gmail.com>
2008-12-01 16:04:34 -0800

    oops not using @config anymore

--- lib/commands/colors.rb
+++ lib/commands/colors.rb
@@ -1,4 +1,4 @@
 Leafman::Command.new "colors", "on|off", "set ANSI colors on or off" do |opt|
     include Leafman::Mixin
-    @config['colors'] = ((opt =~ /^on$/i) ? true : false)
+    Leafman.config['colors'] = ((opt =~ /^on$/i) ? true : false)
 end
