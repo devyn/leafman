8a4de8d7dbb891e967a495a7963a42340fae1f88
devyn <devyn.cairns@gmail.com>
2008-12-18 19:10:35 -0800

    [addon] github-auto-fork finally working + added a little hack so the bot appears to be Linux Mozilla, so GitHub doesn't know it's a bot.

--- addons/github-auto-fork.rb
+++ addons/github-auto-fork.rb
@@ -5,6 +5,7 @@ Leafman::Command.new 'github-auto-fork', '<user>/<repo>', 'automatic github repo
   include Leafman::Mixin
   puts "\e[1mgithub-auto-fork\e[0m"
   agent = WWW::Mechanize.new
+  agent.user_agent_alias = 'Linux Mozilla' # just a hack to get this to work
   puts "\e[1mconnecting to GitHub\e[0m"
   main_page = agent.get 'http://github.com/'
   puts "\e[1mfetching the login page\e[0m"
@@ -17,17 +18,15 @@ Leafman::Command.new 'github-auto-fork', '<user>/<repo>', 'automatic github repo
   login_form['password'] = $stdin.readline.chomp
   puts "\e[1mlogging in...\e[0m"
   login_form.submit
-  puts "\e[1mfetching repo page\e[0m"
-  repo_page = agent.get '/#{user_and_repo}'
   puts "\e[1mforking repo\e[0m"
-  repo_page.links.select{|l|l.text=~/^fork$/i}.first.click
+  agent.get "/#{user_and_repo}/fork"
   puts "waiting 30 seconds to be sure that repo is forked..."
   sleep 30
   puts "\e[1mcloning fork\e[0m"
-  system 'git', 'clone', "git://github.com/#{me}/#{user_and_repo.split('/').last}", File.join(File.expand_path(Leafman::PROJECT_DIR), user_and_repo.split('/').last)
+  system 'git', 'clone', "git@github.com:#{me}/#{user_and_repo.split('/').last}.git", File.join(File.expand_path(Leafman::PROJECT_DIR), user_and_repo.split('/').last)
   Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), user_and_repo.split('/').last)) { system 'git', 'remote', 'add', 'forked_from', "git://github.com/#{user_and_repo}.git" }
   puts "\e[1madding project to database\e[0m"
-  Leafman::Projects.add user_and_repo.split('/').last, 'scm' => 'git', 'type' => nil, 'fetch' => 'forked_from'
+  Leafman::Projects.add user_and_repo.split('/').last, 'scm' => 'git', 'type' => nil, 'fetch' => 'forked_from', 'do_push' => true
   puts "\e[1m\e[32mdone!\e[0m"
 end
 ### ADD-ON (github-auto-fork) END   ###
