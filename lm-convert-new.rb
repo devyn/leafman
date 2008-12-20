6ae8eccd5b677b28361d7a418c94ac166db76544
devyn <devyn.cairns@gmail.com>
2008-12-20 13:45:32 -0800

    [lm-convert-new] fixed syntax errors

--- lm-convert-new.rb
+++ lm-convert-new.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 # Leafman converter from in-project-dir configs to one-dir configs
-base_dir = File.expand_path(ENV['LEAFMAN_DIR'] or "~/Projects")
+base_dir = File.expand_path(ENV['LEAFMAN_DIR']||"~/Projects")
 puts "moving config file"
 File.rename File.join(base_dir, '.leafman'), File.join(base_dir, '.leafman-conf')
 Dir.mkdir File.join(base_dir, '.leafman')
 File.rename File.join(base_dir, '.leafman-conf'), File.join(base_dir, '.leafman', '_config')
 Dir.glob(File.join(base_dir, '*', '.leafman-project')).each do |f|
     puts "processing #{File.basename(File.dirname(f))}"
-    File.rename f, File.join(base_dir, '.leafman', "#{File.dirname(f)}.yml")
+    File.rename f, File.join(base_dir, '.leafman', "#{File.basename(File.dirname(f))}.yml")
 end
