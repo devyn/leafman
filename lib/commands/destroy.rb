6256573b34be08199751c747dd0ef55776e83d2c
devyn <devyn.cairns@gmail.com>
2008-12-20 17:04:59 -0800

    [destroy] bugfix: removes project entry now

--- lib/commands/destroy.rb
+++ lib/commands/destroy.rb
@@ -3,6 +3,7 @@ Leafman::Command.new "destroy", "<project-name>", "destroy the project called <p
     puts "\e[1mdestroy:\e[0m #{pname}"
     p = Leafman::Projects.find(pname)
     warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
-    FileUtils.rm_rf File.join(File.expand_path(Leafman::PROJECT_DIR), pname), :verbose => true
+    FileUtils.rm_rf p.dir, :verbose => true
+    FileUtils.rm    p.conf_file_path, :verbose => true
     puts "\e[32m\e[1mdone!\e[0m"
 end
