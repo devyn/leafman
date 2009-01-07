4d6b25874a7405c7a9a2a5be57d2036636ef4804
devyn <devyn.cairns@gmail.com>
2009-01-06 16:03:11 -0800

    [push] pretty colors

--- lib/commands/push.rb
+++ lib/commands/push.rb
@@ -2,7 +2,19 @@ Leafman::Command.new "push", "[project-names...]", "pushes all distributed enabl
     include Leafman::Mixin
     Leafman::Projects.each(*pnames) do |p|
         if p['scm'] and (p['scm'] != 'svn') and p['do_push']
-            puts "\e[1mpush:\e[0m #{p['name']}"
+            case p['scm']
+                when 'git'
+                    scm_code = "\e[32m"
+                when 'svn'
+                    scm_code = "\e[34m"
+                when 'bzr'
+                    scm_code = "\e[33m"
+                when 'hg'
+                    scm_code = "\e[36m"
+                when 'darcs'
+                    scm_code = "\e[35m"
+            end
+            puts "\e[1mpush:\e[0m #{scm_code}#{p['name']}\e[0m"
             scm_spec = []
             scm_spec << 'origin' if p['scm'] == 'git'
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
