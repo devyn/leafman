3b5e31db2cef382c000c8fd7ba327bcfb6014889
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/git-fork.rb] prettyized

--- lib/commands/git-fork.rb
+++ lib/commands/git-fork.rb
@@ -1,9 +1,9 @@
 Leafman::Command.new "git-fork", "<project-name> <git-url>", "fork the Git project called <project-name> at <git-url>" do |pname, git_url|
     include Leafman::Mixin
-    puts "\e[1mgit-fork:\e[0m #{pname} \e[1mfrom:\e[0m #{git_url}"
-    puts "\e[1mclone\e[0m #{git_url} \e[1minto\e[0m #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
-    warn("\e[31m\e[1mgit clone failed.\e[0m")||true&&next unless system("git", "clone", "-o", "forked_from", git_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
-    puts "\e[1mcreate project config\e[0m"
+    title "git-fork: #{pname} from: #{git_url}"
+    task "clone #{git_url} into #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
+    error("git clone failed.")||true&&next unless system("git", "clone", "-o", "forked_from", git_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
+    task "create project config"
     Leafman::Projects.add(pname, 'type' => nil, 'scm' => 'git', 'fetch' => 'forked_from')
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
