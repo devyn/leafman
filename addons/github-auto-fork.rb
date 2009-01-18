797343410b48884931ddbb5589f755577660dd24
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 14:04:23 -0800

    [addons:github-auto-fork] using pretty builtins (except for HighLine)

--- addons/github-auto-fork.rb
+++ addons/github-auto-fork.rb
@@ -1,7 +1,7 @@
 ### ADD-ON (github-auto-fork) BEGIN ###
 # GitHub auto fork for Leafman, using WWW::Mechanize, and HighLine for asking questions and stuff.
 require 'mechanize'
-require 'highline/import'
+require 'highline'
 class << $stdin
   def eof?
     false
@@ -9,28 +9,30 @@ class << $stdin
 end
 Leafman::Command.new 'github-auto-fork', '<user>/<repo>', 'automatic github repo forking', 'official addons' do |user_and_repo|
   include Leafman::Mixin
-  puts "\e[1mgithub-auto-fork\e[0m"
+  title "github-auto-fork"
   agent = WWW::Mechanize.new
   agent.user_agent_alias = 'Linux Mozilla' # just a hack to get this to work
-  puts "\e[1mconnecting to GitHub\e[0m"
+  task "connecting to GitHub"
   main_page = agent.get 'http://github.com/'
-  puts "\e[1mfetching the login page\e[0m"
+  task "fetching the login page"
   login_page = main_page.links.select{|l|l.text=~/^login$/i}.first.click
   login_form = login_page.forms.first
   puts "Don't worry, this information doesn't go anywhere."
-  me = ask('Your GitHub username? '); login_form['login'] = me
-  login_form['password'] = ask('Your GitHub password? ') {|q| q.echo = '*' }
-  puts "\e[1mlogging in...\e[0m"
+  me = $terminal.ask('Your GitHub username? '); login_form['login'] = me
+  login_form['password'] = $terminal.ask('Your GitHub password? ') {|q| q.echo = '*' }
+  task "logging in"
   login_form.submit
-  puts "\e[1mforking repo\e[0m"
+  task "forking repo"
   agent.get "/#{user_and_repo}/fork"
-  puts "waiting 15 seconds to be sure that repo is forked..."
+  log "waiting 15 seconds to be sure that repo is forked..."
   sleep 15
-  puts "\e[1mcloning fork\e[0m"
+  task "cloning fork"
+  command 'git clone ...'
   system 'git', 'clone', "git@github.com:#{me}/#{user_and_repo.split('/').last}.git", File.join(File.expand_path(Leafman::PROJECT_DIR), user_and_repo.split('/').last)
+  command 'git remote add ...'
   Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), user_and_repo.split('/').last)) { system 'git', 'remote', 'add', 'forked_from', "git://github.com/#{user_and_repo}.git" }
-  puts "\e[1madding project to database\e[0m"
+  task "adding project to database"
   Leafman::Projects.add user_and_repo.split('/').last, 'scm' => 'git', 'type' => nil, 'fetch' => 'forked_from', 'do_push' => true
-  puts "\e[1m\e[32mdone!\e[0m"
+  finished
 end
 ### ADD-ON (github-auto-fork) END   ###
