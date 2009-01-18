551453d1912cb2633bc84ae4415d30b5bca35650
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/hg-get.rb] prettyized

--- lib/commands/hg-get.rb
+++ lib/commands/hg-get.rb
@@ -1,9 +1,9 @@
 Leafman::Command.new "hg-get", "<project-name> <hg-url>", "clone the Mercurial project called <project-name> at <hg-url>" do |pname, hg_url|
     include Leafman::Mixin
-    puts "\e[1mhg-get:\e[0m #{pname} \e[1mfrom:\e[0m #{hg_url}"
-    puts "\e[1mclone\e[0m #{hg_url} \e[1minto\e[0m #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
-    return warn("\e[31m\e[1mhg clone failed.\e[0m") unless system("hg", "clone", hg_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
-    puts "\e[1mcreate project config\e[0m"
+    title "hg-get: #{pname} from: #{hg_url}"
+    task "clone #{hg_url} into #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
+    return error("hg clone failed.") unless system("hg", "clone", hg_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
+    task "create project config"
     Leafman::Projects.add(pname, 'type' => nil, 'scm' => 'hg', 'do_pull' => true)
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
