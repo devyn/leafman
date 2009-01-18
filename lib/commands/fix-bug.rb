070771573c623cf084bd53d9393566b0761f1a66
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/fix-bug.rb] prettyized

--- lib/commands/fix-bug.rb
+++ lib/commands/fix-bug.rb
@@ -1,12 +1,12 @@
 Leafman::Command.new "fix-bug", "<project-name> <number>", "fix a bug on <project-name> with <number> (ex. b0, b1, b2...)" do |pname, num|
     include Leafman::Mixin
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
+    error("project not found.")||true&&next unless p
     if num =~ /^b(\d+)$/i
         p['bugs'] -= [p['bugs'][$1.to_i]] if p['bugs']
     elsif num =~ /^t(\d+)$/i
         p['todos'] -= [p['todos'][$1.to_i]] if p['todos']
     else
-        warn("\e[31m\e[1minvalid number.\e[0m")||true&&next
+        error("invalid number.")||true&&next
     end
 end
