396547743af4634f4fa56671fb4c327bd59b05a8
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/push.rb] prettyized

--- lib/commands/push.rb
+++ lib/commands/push.rb
@@ -1,8 +1,9 @@
 Leafman::Command.new "push", "[project-names...]", "pushes all distributed enabled projects or [project-names...] to the server" do |*pnames|
     include Leafman::Mixin
+    title "pushing projects"
     Leafman::Projects.each(*pnames) do |p|
         if p['scm'] and (p['scm'] != 'svn') and p['do_push']
-            puts "\e[1mpush:\e[0m #{p.scm_color}#{p['name']}\e[0m"
+            task "push: #{p.scm_color}#{p['name']}\e[0m"
             scm_spec = []
             scm_spec << 'origin' if p['scm'] == 'git'
             Dir.chdir(File.join(File.expand_path(Leafman::PROJECT_DIR), p['name'])) do
@@ -10,5 +11,5 @@ Leafman::Command.new "push", "[project-names...]", "pushes all distributed enabl
             end
         end
     end
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
