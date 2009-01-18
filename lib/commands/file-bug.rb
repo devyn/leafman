aa66860e96af118fba0349e53a2f311c0747f264
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/file-bug.rb] prettyized

--- lib/commands/file-bug.rb
+++ lib/commands/file-bug.rb
@@ -1,7 +1,7 @@
 Leafman::Command.new "file-bug", "<project-name> <bug>", "Add a bug to <project-name> -- this shows up on what-to-do and show" do |pname, bug|
     include Leafman::Mixin
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
+    error("project not found.")||true&&next unless p
     p['bugs'] = [] unless p['bugs']
     p['bugs'] += [bug]
 end
