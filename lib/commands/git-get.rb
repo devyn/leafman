257c28038eba6272c4591d7c75c3529f0563746f
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/git-get.rb] prettyized

--- lib/commands/git-get.rb
+++ lib/commands/git-get.rb
@@ -1,9 +1,9 @@
 Leafman::Command.new "git-get", "<project-name> <git-url>", "clone the Git project called <project-name> at <git-url>" do |pname, git_url|
     include Leafman::Mixin
-    puts "\e[1mgit-get:\e[0m #{pname} \e[1mfrom:\e[0m #{git_url}"
-    puts "\e[1mclone\e[0m #{git_url} \e[1minto\e[0m #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
-    return warn("\e[31m\e[1mgit clone failed.\e[0m") unless system("git", "clone", git_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
-    puts "\e[1mcreate project config\e[0m"
+    title "git-get: #{pname} from: #{git_url}"
+    task "clone #{git_url} into #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
+    return error("git clone failed.") unless system("git", "clone", git_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
+    task "create project config"
     Leafman::Projects.add(pname, 'type' => nil, 'scm' => 'git', 'fetch' => 'origin')
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
