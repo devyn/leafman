7d0115ad83441ce5a0bef441b975b10df33e4bc2
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:43 -0800

    [lib/commands/bzr-init.rb] prettyized

--- lib/commands/bzr-init.rb
+++ lib/commands/bzr-init.rb
@@ -1,14 +1,14 @@
 Leafman::Command.new "bzr-init", "<project-name>", "initialize <project-name> as a Bazaar repository" do |pname|
     include Leafman::Mixin
-    puts "\e[1mbzr-init:\e[0m #{pname}"
+    title "bzr-init: #{pname}"
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
-    puts "\e[1mchdir\e[0m #{p.dir}"
+    error("project not found.")||true&&next unless p
+    command "chdir #{p.dir}"
     Dir.chdir(p.dir) do
-        puts "\e[1mbzr init\e[0m"
-        system('bzr', 'init') or (warn("\e[31mfailed to initialize.\e[0m")||true&&next)
+        command "bzr init"
+        system('bzr', 'init') or (error("failed to initialize.")||true&&next)
     end
-    puts "\e[1mset SCM to BZR\e[0m"
+    task "set SCM to BZR"
     p['scm'] = 'bzr'
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
