a3126ee1483ef73ae0f0ff3fd4b4e38612358b00
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:45 -0800

    [lib/commands/set-push.rb] prettyized

--- lib/commands/set-push.rb
+++ lib/commands/set-push.rb
@@ -1,8 +1,8 @@
 Leafman::Command.new "set-push", "<project-name> on|off", "choose whether <project-name> is push-enabled or not", "configuration" do |pname, opt|
     include Leafman::Mixin
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
-    warn("\e[31m\e[1mproject does not use a valid SCM.\e[0m")||true&&next unless p['scm']
-    warn("\e[31m\e[1mproject uses a non-distibuted SCM.\e[0m")||true&&next if p['scm'] == 'svn'
+    error("project not found.")||true&&next unless p
+    error("project does not use a valid SCM.")||true&&next unless p['scm']
+    error("project uses a non-distibuted SCM.")||true&&next if p['scm'] == 'svn'
     p['do_push'] = opt =~ /^on$/i ? true : false
 end
