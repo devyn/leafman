9bcc54632ebc74b9da582adf2d487952e07770ba
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:43 -0800

    [lib/commands/darcs-get.rb] prettyized

--- lib/commands/darcs-get.rb
+++ lib/commands/darcs-get.rb
@@ -1,9 +1,9 @@
 Leafman::Command.new "darcs-get", "<project-name> <hg-url>", "get the Darcs project called <project-name> at <darcs-url>" do |pname, hg_url|
     include Leafman::Mixin
-    puts "\e[1mdarcs-get:\e[0m #{pname} \e[1mfrom:\e[0m #{darcs_url}"
-    puts "\e[1mget\e[0m #{hg_url} \e[1minto\e[0m #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
-    return warn("\e[31m\e[1mdarcs get failed.\e[0m") unless system("darcs", "get", hg_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
-    puts "\e[1mcreate project config\e[0m"
+    title "darcs-get: #{pname} from: #{darcs_url}"
+    task "get #{hg_url} into #{File.join(File.expand_path(Leafman::PROJECT_DIR), pname)}"
+    return error("darcs get failed.") unless system("darcs", "get", hg_url, File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
+    task "create project config"
     Leafman::Projects.add(pname, 'type' => nil, 'scm' => 'darcs', 'do_pull' => true)
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
