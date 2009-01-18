6496eff3d37b6f3939365bf4d4b0c364e0ed98b0
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/redetect.rb] prettyized

--- lib/commands/redetect.rb
+++ lib/commands/redetect.rb
@@ -1,8 +1,9 @@
 Leafman::Command.new "redetect", "[project-names...]", "redetect information about a project based on its contents" do |*projs|
   include Leafman::Mixin
+  task 'redetect project information'
   Leafman::Projects.each(*projs) do |p|
-    puts "processing #{p['name']}."
+    log "processing #{p['name']}."
     p.detect
   end
-  puts "\e[1m\e[32mdone!\e[0m"
+  finished
 end
