5f501a3c4eba3cb13b80826675556dee12eaf58d
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:35:18 -0800

    [pretty] more functions

--- lib/pretty.rb
+++ lib/pretty.rb
@@ -2,7 +2,7 @@
 # Edit as you wish.
 module Leafman::Mixin
     def title s
-        puts "*** \e[1m#{s}\e[0m ***"
+        puts ":: \e[34m\e[1m#{s}\e[0m"
     end
     def task s
         puts ":: \e[36m\e[1m#{s}\e[0m"
@@ -18,6 +18,12 @@ module Leafman::Mixin
     end
     def list_item s, bullet="**"
         puts "\e[1m#{bullet}\e[0m        #{s}"
+    end
+    def log s
+        puts "::     #{s}"
+    end
+    def ask s
+        puts "??     \e[36m#{s}\e[0m"
     end
     def finished
         puts ":: \e[32m\e[1mdone!\e[0m"
