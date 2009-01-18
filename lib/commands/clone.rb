e9dce20c3cd5db167d39cdf5aeed708b499d0dc7
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:43 -0800

    [lib/commands/clone.rb] prettyized

--- lib/commands/clone.rb
+++ lib/commands/clone.rb
@@ -5,48 +5,48 @@ Leafman::Command.new "clone", "<project-name> [host[:port]]", "clones a project 
     include Leafman::Mixin
     begin
         as = (a[0] or 'localhost').split(':')
-        puts "\e[1mclone:\e[0m #{pname} \e[1mat:\e[0m #{a[0] or 'localhost'}"
-        puts "\e[1mfetching project SCM\e[0m"
+        title "clone: #{pname} at: #{a[0] or 'localhost'}"
+        task "fetching project SCM"
         case open(Synkage.escape("http://localhost:8585/#{pname}.project")).read.scan(/class='scm-(\w+)'/).flatten.first
         when 'git'
-            puts "\e[1mgit clone\e[0m"
+            command "git clone"
             system "git", "clone", Synkage.escape("http://#{as[0]}:#{as[1] or 8585}/#{pname}.project/files/.git/"), File.join(File.expand_path(Leafman::PROJECT_DIR), pname)
         when 'svn'
-            warn "\e[31mSorry, no Subversion support.\e[0m"
+            error "Sorry, no Subversion support."
             next
         when 'bzr'
-            puts "\e[1mbzr checkout\e[0m"
+            command "bzr checkout"
             system "bzr", "co", Synkage.escape("http://#{as[0]}:#{as[1] or 8585}/#{pname}.project/files/"), File.join(File.expand_path(Leafman::PROJECT_DIR), pname)
         when 'hg'
-            warn "\e[31mSorry, no Mercurial support. (yet!)\e[0m"
+            error "Sorry, no Mercurial support. (yet!)"
             next
         when 'darcs'
-            puts "\e[1mdarcs get --lazy\e[0m"
+            command "darcs get --lazy"
             system "darcs", "get", "--lazy", Synkage.escape("http://#{as[0]}:#{as[1] or 8585}/#{pname}.project/files/.git/"), File.join(File.expand_path(Leafman::PROJECT_DIR), pname)
         else
             sy = Synkage.new("http://#{as[0]}:#{as[1] or 8585}/#{pname}.project/files/", File.join(File.expand_path(Leafman::PROJECT_DIR), pname))
-            puts "\e[1mfetching a list of all files on server...\e[0m"
+            task "fetching a list of all files on server"
             ws = sy.fetch_whats
-            puts "\e[1mmaking the folder structure...\e[0m"
+            task "making the folder structure"
             sy.expand_dir_for *ws
-            puts "\e[1mbeginning file download...\e[0m"
+            task "beginning file download"
             ws.each do |w|
                 unless sy.up_to_date?(w)
-                    puts "\e[1mfetching\e[0m #{w}"
+                    command "fetching #{w}"
                     sy.download w
                 end
             end
         end
-        puts "\e[1mcreate project config\e[0m"
+        task "create project config"
         Leafman::Projects.add(pname, {})
-        puts "\e[1mdetect project information\e[0m"
+        task "detect project information"
         Leafman::Projects.find(pname).detect
-        puts "\e[1m\e[32mdone!\e[0m"
+        finished
     rescue Exception # anything
         if $DEBUG
             raise $!
         else
-            warn("\e[1m\e[31mERROR:\e[0m\e[31m #{$!.message}\e[0m")||true&&next
+            error($!.message)||true&&next
         end
     end
 end
