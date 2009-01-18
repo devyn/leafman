d1de8a92e649aabab497dfce395d7fd5e39f5313
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:45 -0800

    [lib/commands/skel-rails.rb] prettyized

--- lib/commands/skel-rails.rb
+++ lib/commands/skel-rails.rb
@@ -1,16 +1,17 @@
 Leafman::Command.new "skel-rails", "<project-name> [rails-options...]", "run 'rails' on the <project-name>'s dir with [rails-options...]" do |pname, *rails_opts|
     include Leafman::Mixin
-    puts "\e[1mskel-rails:\e[0m #{pname}"
+    title "skel-rails: #{pname}"
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
-    puts "\e[1mrun \e[0m'rails'\e[1m on project dir\e[0m"
+    error("project not found.")||true&&next unless p
+    command "chdir \"#{p.dir}\""
     Dir.chdir(p.dir) do
         run = ["rails"]
         run += rails_opts
         run << "."
+        command run.join(' ')
         system *run
     end
-    puts "\e[1mchange type to\e[0m rails"
+    task "change type to rails"
     p['type'] = 'rails'
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
