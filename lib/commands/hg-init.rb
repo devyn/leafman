7b887d55b58f64580832e376b1bd7a2f3fe50dd1
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/hg-init.rb] prettyized

--- lib/commands/hg-init.rb
+++ lib/commands/hg-init.rb
@@ -1,14 +1,14 @@
 Leafman::Command.new "hg-init", "<project-name>", "initialize <project-name> as a Mercurial repository" do |pname|
     include Leafman::Mixin
-    puts "\e[1mhg-init:\e[0m #{pname}"
+    title "hg-init: #{pname}"
     p = Leafman::Projects.find(pname)
-    warn("\e[31m\e[1mproject not found.\e[0m")||true&&next unless p
-    puts "\e[1mchdir\e[0m #{p.dir}"
+    error("project not found.")||true&&next unless p
+    command "chdir #{p.dir}"
     Dir.chdir(p.dir) do
-        puts "\e[1mhg init\e[0m"
-        system('hg', 'init') or (warn("\e[31mfailed to initialize.\e[0m")||true&&next)
+        command "hg init"
+        system('hg', 'init') or (error("failed to initialize.")||true&&next)
     end
-    puts "\e[1mset SCM to hg\e[0m"
+    task "set SCM to hg"
     p['scm'] = 'hg'
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
