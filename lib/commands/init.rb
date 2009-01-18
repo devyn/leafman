8e67ba58c8daca7b3392aa9f53acb7f51c6b18d7
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/init.rb] prettyized

--- lib/commands/init.rb
+++ lib/commands/init.rb
@@ -1,10 +1,10 @@
 Leafman::Command.new "init", "", "initialize the project directories for first time" do
     include Leafman::Mixin
     epath = File.expand_path Leafman::PROJECT_DIR
-    puts "\e[1minit:\e[0m #{epath}"
-    abort("#{File.join(epath, '.leafman')}: \e[31m\e[1mdir already exists. refusing to init.\e[0m") if File.directory?(File.join(epath, '.leafman'))
+    title "init: #{epath}"
+    error("#{File.join(epath, '.leafman')}: dir already exists. refusing to init.")||true&&exit(1) if File.directory?(File.join(epath, '.leafman'))
     FileUtils.mkdir_p File.join(epath, '.leafman'), :verbose => true
-    puts "\e[1mconfig skeleton\e[0m"
+    task "config skeleton"
     @config = {}
-    puts "\e[32m\e[1mdone!\e[0m"
+    finished
 end
