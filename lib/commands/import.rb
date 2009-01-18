a94e2e590e4886f00d280ae38b156f7ddc485a9d
Devyn Cairns <devyn.cairns@gmail.com>
2009-01-18 13:37:44 -0800

    [lib/commands/import.rb] prettyized

--- lib/commands/import.rb
+++ lib/commands/import.rb
@@ -1,11 +1,11 @@
 Leafman::Command.new "import", "<directory>", "import <directory> as a project, then auto-detect it's SCM" do |dir|
     include Leafman::Mixin
-    puts "\e[1mimport:\e[0m #{File.basename(dir)}"
+    title "import: #{File.basename(dir)}"
     FileUtils.cp_r File.expand_path(dir), File.join(File.expand_path(Leafman::PROJECT_DIR), File.basename(dir)), :verbose => true
-    puts "\e[1mcreating project config\e[0m"
+    task "creating project config"
     p = Leafman::Projects.add(File.basename(dir), 'type' => nil, 'scm' => nil)
-    puts "\e[1mauto-detecting project info\e[0m"
+    task "auto-detecting project info"
     p.detect
-    puts *p.ro_hash.map{|k,v|"#{k}: #{v.inspect}"}
-    puts "\e[32m\e[1mdone!\e[0m"
+    p.ro_hash.each{|k,v|list_item "#{k}: #{v.inspect}"}
+    finished
 end
