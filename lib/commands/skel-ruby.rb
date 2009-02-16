553f865f88db96f56282b2aba678c06c1bd8045f
Devyn Cairns <devyn.cairns@gmail.com>
2009-02-15 19:28:08 -0800

    [command:skel-ruby] in separate files

--- lib/commands/skel-ruby.rb
+++ lib/commands/skel-ruby.rb
@@ -9,24 +9,45 @@ Leafman::Command.new "skel-ruby", "<project-name> [classes...]", "generate a Rub
     error("project not found.")||true&&next  unless p
     title "generate Ruby skeleton for #{p['name']}"
     require 'ext/ruby_builder'
-    code = RubyBuilder.new do
-        namespace p['name'].rubyize do
-            classes.each do |clsu|
-                cls = clsu.split(":")
-                task "adding class stub #{cls[0]}"
-                type cls[0] do
-                    cls[1..-1].each do |m|
-                        if m =~ /^@/
-                            log "adding attribute stub #{cls[0]}##{m.sub(/^@/,'')}"
-                            self << "attr_accessor #{m.sub(/^@/,':')}"
-                        else
-                            log "adding method stub #{cls[0]}##{m}"
-                            method(m) { comment "TODO: write this code (#{m})" }
+    Dir.mkdir(p.dir('lib'))
+    files = []
+    classes.each do |clsu|
+        cls = clsu.split(":")
+        task "adding class stub for: #{p['name'].rubyize}::#{cls[0].rubyize}"
+        files << File.join('lib', "#{cls[0]}.rb")
+        File.open(p.dir(files[-1]), 'w') do |f|
+            code = RubyBuilder.new do
+                namespace p['name'].rubyize do
+                    type cls[0].rubyize do
+                        cls[1..-1].each do |m|
+                            if m =~ /^@/
+                                log "adding attribute: #{m}"
+                                self << "attr_accessor #{m.sub(/^@/, ':')}"
+                            else
+                                log "adding method:    #{m}"
+                                method(m) { comment "TODO: write action code for #{m}" }
+                            end
                         end
                     end
                 end
-            end
+            end.build
+            f.write code
         end
-    end.build
-    File.open(p.dir("#{p['name']}.rb"), 'w') {|f| f.write code}
+    end
+    File.open(p.dir("#{p['name']}.rb"), 'w') do |f|
+        task "generating library index"
+        code = RubyBuilder.new do
+            comment "code skeleton generated via Leafman (http://www.github.com/devyn/leafman)"
+            namespace p['name'].rubyize do
+                self << 'extend self'
+            end
+            self << ''
+            self << "$: << File.expand_path(File.dirname(__FILE__))"
+            files.each do |fl|
+                log "- #{fl}"
+                self << "require #{fl.inspect}"
+            end
+        end.build
+        f.write code
+    end
 end
