ad70406af7a493cdd616b2d9e071cc68eb407426
Devyn Cairns <devyn.cairns@gmail.com>
2009-02-16 09:48:10 -0800

    [serve] fixed 'no method (title)' bug

--- lib/commands/serve.rb
+++ lib/commands/serve.rb
@@ -1,5 +1,6 @@
 require 'webrick'
 Leafman::Command.new "serve", "[port]", "set up an HTTPServer for all projects (optionally, on [port] or 8585)" do |*args|
+    include Leafman::Mixin
     title "Leafman WEB serve"
     serv = WEBrick::HTTPServer.new(:Port => (args.shift or 8585))
     serv.mount("/", Leafman::Servlet)
