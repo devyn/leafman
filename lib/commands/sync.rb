54af00c485eedc268dfd58584288e24b98c5f4b9
Devyn Cairns <devyn.cairns@gmail.com>
2009-03-10 17:11:06 -0700

    [commands] more run_command fixes * lib/commands/sync.rb: added run_command instead of system, now using  instead.

--- lib/commands/sync.rb
+++ lib/commands/sync.rb
@@ -6,27 +6,27 @@ Leafman::Command.new "sync", "[project-names...]", "syncs all enabled projects o
         if (p['scm'] == 'git') and p['fetch']
             task "sync: \e[32m#{p['name']}\e[0m"
             Dir.chdir(p.dir) do
-                system("git", "pull", p['fetch'], "master") or error("could not pull for #{p['name']}")
+                run_command("git", "pull", p['fetch'], "master") or error("could not pull for #{p['name']}")
             end
         elsif (p['scm'] == 'svn') and p['do_update']
             task "sync: \e[34m#{p['name']}\e[0m"
             Dir.chdir(p.dir) do
-                system("svn", "up") or error("could not update #{p['name']}")
+                run_command("svn", "up") or error("could not update #{p['name']}")
             end
         elsif (p['scm'] == 'bzr') and p['do_update']
             task "sync: \e[33m#{p['name']}\e[0m"
             Dir.chdir(p.dir) do
-                system("bzr", "up") or error("could not update #{p['name']}")
+                run_command("bzr", "up") or error("could not update #{p['name']}")
             end
         elsif (p['scm'] == 'hg') and p['do_pull']
             task "sync: \e[36m#{p['name']}\e[0m"
             Dir.chdir(p.dir) do
-                system("hg", "pull") and system("hg", "update") or error("could not pull for #{p['name']}")
+                run_command("hg", "pull", "-u") or error("could not pull for #{p['name']}")
             end
         elsif (p['scm'] == 'darcs') and p['do_pull']
             task "sync: \e[35m#{p['name']}\e[0m"
             Dir.chdir(p.dir) do
-                system("darcs", "pull", '-a') or error("could not pull for #{p['name']}")
+                run_command("darcs", "pull", '-a') or error("could not pull for #{p['name']}")
             end
         elsif p['synkage_url']
             task "sync: \e[0m#{p['name']}"
