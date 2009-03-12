c82480ba651a889e09fce805b37a8d6d1997c111
Devyn Cairns <devyn.cairns@gmail.com>
2009-03-11 18:55:12 -0700

    [core] detection engine simplified type * lib/detection.rb: changed the detection type

--- lib/detection.rb
+++ lib/detection.rb
@@ -1,65 +1,46 @@
 module Leafman
-  class Projects::Accessor
-    def detect
-      # detect SCM
-      self['scm'] =  nil
-      self['scm'] = 'git'   if File.directory?(dir('.git'  ))
-      self['scm'] = 'svn'   if File.directory?(dir('.svn'  ))
-      self['scm'] = 'bzr'   if File.directory?(dir('.bzr'  ))
-      self['scm'] = 'hg'    if File.directory?(dir('.hg'   ))
-      self['scm'] = 'darcs' if File.directory?(dir('_darcs'))
-      # detect TYPE
-      rb    = 0
-      py    = 0
-      java  = 0
-      js    = 0
-      pl    = 0
-      c     = 0
-      cpp   = 0
-      other = 0
-      
-      goproc = proc do |d|
-        (Dir.entries(d) - %w(. .. .git .svn .bzr .hg _darcs)).each do |e|
-          if File.file?(File.join(d, e))
-            case e.split('.').last.downcase
-              when 'rb'
-                rb    += 1
-              when 'py'
-                py    += 1
-              when 'java'
-                java  += 1
-              when 'js'
-                js    += 1
-              when 'pl'
-                pl    += 1
-              when 'c', 'h'
-                c     += 1
-              when 'cc', 'hh', 'cpp', 'hpp', 'c++', 'h++'
-                cpp   += 1
-              else
-                other += 1
+    class Projects::Accessor
+        def detect
+            # detect SCM
+            self['scm'] =  nil
+            self['scm'] = 'git'   if File.directory?(dir('.git'  ))
+            self['scm'] = 'svn'   if File.directory?(dir('.svn'  ))
+            self['scm'] = 'bzr'   if File.directory?(dir('.bzr'  ))
+            self['scm'] = 'hg'    if File.directory?(dir('.hg'   ))
+            self['scm'] = 'darcs' if File.directory?(dir('_darcs'))
+            # detect TYPE
+            typecount = {}
+            Dir.chdir self.dir do
+                typecount = {'ruby'       => Dir.glob('**/*.rb').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'python'     => Dir.glob('**/*.{py,pyc}').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'java'       => Dir.glob('**/*.{java,class}').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'javascript' => Dir.glob('**/*.js').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'pl'         => Dir.glob('**/*.pl').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'c'          => Dir.glob('**/*.c').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'cpp'        => Dir.glob('**/*.{cc,cpp,c++}').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count,
+                             'erl'        => Dir.glob('**/*.{erl,beam}').reject{|i|i=~/^(_darcs|\.git|\.svn|\.bzr|\.hg)/}.count
+                            }
+               typecount[nil] = Dir.glob(%w(**/*[^/] *[^/])).count - typecount.values.flatten.count
             end
-          else
-            goproc.call File.join(d, e)
-          end
+            if eval(typecount.reject{|k,v|(k == nil) or (v == 0)}.values.join('+'))
+                self['type'] = nil
+            else
+                self['type'] = typecount.sort{|a1,a2| a1[1] <=> a2[1]}.first[0]
+            end
+            # detect SYNC
+            case self['scm']
+                when 'git'
+                    self['fetch'] = Dir.chdir(dir) { `git remote`.split("\n").first }
+                when 'svn'
+                    self['do_update'] = true
+                when 'bzr'
+                    self['do_update'] = Dir.chdir(dir) { `bzr info`.scan(/^\s*checkout of branch:/i).size > 0 }
+                when 'hg'
+                    self['do_pull'] = Dir.chdir(dir) { `hg showconfig paths.default`.size > 0 }
+                when 'darcs'
+                    self['do_pull'] = Dir.chdir(dir) { `darcs show repo`.scan(/^\s*Default Remote/i).size > 0 }
+            end
+            self
         end
-      end
-      goproc.call dir
-      self['type'] = {'ruby' => rb, 'python' => py, 'java' => java, 'javascript' => js, 'perl' => pl, 'c' => c, 'c++' => cpp, nil => other}.sort{|a1,a2|a2[1] <=> a1[1]}.first[0]
-      # detect SYNC.
-      case self['scm']
-        when 'git'
-          self['fetch'] = Dir.chdir(dir) { `git remote`.split("\n").first }
-        when 'svn'
-          self['do_update'] = true
-        when 'bzr'
-          self['do_update'] = Dir.chdir(dir) { `bzr info`.scan(/^\s*checkout of branch:/i).size > 0 }
-        when 'hg'
-          self['do_pull'] = Dir.chdir(dir) { `hg showconfig paths.default`.size > 0 }
-        when 'darcs'
-          self['do_pull'] = Dir.chdir(dir) { `darcs show repo`.scan(/^\s*Default Remote/i).size > 0 }
-      end
-      self
     end
-  end
 end
